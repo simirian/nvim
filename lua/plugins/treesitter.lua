de1fc6fc7ec871f5772f3e4322c1b35662f285a7
simirian <simirian5813@gmail.com>
2024-09-16 11:12:58 -0400

    fix: treesitter buggy behavior
    Treesitter would install redundant parsers, so I removed that with a
    call to `nvim_get_runtime_files()`. Also stopped it from stync
    installing. IDK why it did that before.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = { "nvim-manager" },
   opts = {
-    sync_install = true,
+    sync_install = false,
     auto_install = false,
     highlight = {
       enable = true,
@@ -13,13 +13,19 @@ return {
     },
     indent = {
       enable = true,
+      disable = { "markdown" },
     },
   },
   config = function(_, opts)
     opts.ensure_installed = opts.ensure_installed or {}
+    local installed = vim.tbl_map(function(e)
+      return vim.fn.fnamemodify(e, ":t:r")
+    end, vim.api.nvim_get_runtime_file("parser/*", true))
     for _, spec in pairs(require("nvim-manager.workspaces").list()) do
       for _, ft in ipairs(spec.filetypes) do
-        if not vim.tbl_contains(opts.ensure_installed, ft) then
+        if not vim.tbl_contains(opts.ensure_installed, ft)
+            and not vim.tbl_contains(installed, ft)
+        then
           table.insert(opts.ensure_installed, ft)
         end
       end
