996a613d4be17ca7c5fc02d3f6d10f4165b28539
simirian <simirian5813@gmail.com>
2024-11-18 21:00:14 -0500

    refactor: updated to work with contour branch
    Updated lines to work with the new contour branch. Pretty much
    effectively the same.

--- lua/plugins/lines.lua
+++ lua/plugins/lines.lua
@@ -1,84 +1,82 @@
 -- simirian's NeoVim
 -- lines icons for statusline and tabline
 
-local vfn = vim.fn
 local icons = require("icons")
 
--- plugin {{{1
+-- statusline components
+local buffer = {
+  "buffer",
+  items = { "typeicon", "filename", "bufnr", "modified" },
+  default_name = "U.N. Owen",
+  modified_icon = icons.dot,
+}
+local diagnostics = {
+  "diagnostics",
+  highlights = {
+    default = "User2",
+  },
+  icons = {
+    error = icons.error,
+    warn = icons.warning,
+    info = icons.info,
+    hint = icons.hint,
+    default = icons.status,
+  },
+}
+local ruler = {
+  "raw",
+  item = "%#User2# %l/%L %v ",
+}
+
+-- tabline components
+local dirclock = {
+  "raw",
+  item = "%#User1# %{fnamemodify(getcwd(), ':t')} | %{strftime('%H:%M')} ",
+}
+local tabbufs = {
+  "tab",
+  items = { "buflist" },
+  buflist = {
+    buffer = {
+      items = { "typeicon", "filename" },
+      default_name = "U.N. Owen",
+    },
+  },
+  highlight_sel = "TabLine",
+}
+local tablist = {
+  "tablist",
+  tab = {
+    "tab",
+    modified_icon = icons.dot,
+  },
+}
+
 return {
   "nvim-contour",
   dev = true,
   opts = {
-    -- buffer {{{2
-    buffer = {
-      filetype = "icon",
-      default_name = "U.N. Owen",
-      modified_icon = icons.dot,
-      show_bufnr = true,
-    },
-    -- buffer list {{{2
-    buflist = {
-      modified_icon = false,
-      show_bufnr = false,
-      filter = function(bufnr)
-        local bi = vfn.getbufinfo(bufnr)[1]
-        return bi.listed == 1 and bi.loaded == 1 and bi.hidden ~= 1
-            and vim.tbl_contains(vfn.tabpagebuflist(vfn.tabpagenr()), bufnr)
-      end,
-    },
-    tablist = {
-      modified_icon = icons.dot,
-      close_icon = icons.cross,
-    },
-    -- diagnostics {{{2
-    diagnostics = {
-      highlight = {
-        error = "ContourError",
-        warn  = "ContourWarn",
-        info  = "ContourInfo",
-        hint  = "ContourHint",
-        base  = "User2",
-      },
-      icons = {
-        error = icons.error,
-        warn  = icons.warning,
-        info  = icons.info,
-        hint  = icons.hint,
-        base  = icons.diagnostics,
-      },
-    },
-    -- statusline {{{2
-    show_statusline = "always",
     statusline = {
-      highlight = "",
-      {
-        highlight = "",
-        left = true,
-        min_width = 10,
-        { component = "diagnostics" },
+      default = {
+        "space",
+        highlight = "StatusLineNC",
+        items = {
+          diagnostics,
+          buffer,
+          ruler,
+        },
       },
-      "%=",
-      { component = "buffer" },
-      "%=",
-      { "%#User2# %l,%c ", min_width = 10 },
     },
-    -- tabline {{{2
-    show_tabline = "always",
     tabline = {
-      highlight = "tablinefill",
-      {
-        left = true,
-        min_width = 30,
-        "%1* %{fnamemodify(getcwd(), ':t')} | %{strftime('%H:%M')} %*",
-      },
-      "%=",
-      { component = "buflist" },
-      "%=",
-      {
-        min_width = 30,
-        { component = "tablist" },
+      default = {
+        "space",
+        highlight = "TabLine",
+        items = {
+          dirclock,
+          tabbufs,
+          tablist,
+        },
       },
     },
   },
 }
--- vim:fdm=marker
