15d5b0bc04f5154930c4c28730ae9ed76803cd2b
simirian <simirian5813@gmail.com>
2025-03-25 19:39:46 -0400

    refactor: minified lazy-init configuration
    Lazy had no right having a large config when I've been planning to
    remove it. It has been reduced to pretty much default.

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -1,49 +1,17 @@
---------------------------------------------------------------------------------
---                              simirian's NeoVim                             --
---                              ~ lazy plugins ~                              --
---------------------------------------------------------------------------------
+-- simirian's NeoVim
+-- lazy.nvim plugins
 
 local vfn = vim.fn
 
--- bootstrap {{{1
 local lazypath = vfn.stdpath("data") .. "/lazy/lazy.nvim"
+--- @diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
   vfn.system { "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, }
 end
 vim.opt.rtp:prepend(lazypath)
 
-local osc = vim.loop.os_getenv("YICKS_THEME")
-local colors = osc and osc or "yicks"
-
--- setup {{{1
 require("lazy").setup("plugins", {
-  dev = { path = "~/Source/" },
-  install = { colorscheme = { colors, "habamax" } },
-  ui = {
-    size = { width = 0.8, height = 0.8 },
-    border = "none",
-    pills = true,
-    icons = { list = { "-", "-", "-", "-", } },
-  },
-  change_detection = { notify = false },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        -- "gzip",
-        -- "matchit",
-        -- "matchparen",
-        "netrwPlugin",
-        -- "tarPlugin",
-        -- "tohtml",
-        -- "tutor",
-        -- "zipPlugin",
-      },
-    },
-  },
-  -- lazy can generate helptags from the headings in markdown readme files,
-  -- so :help works even for plugins that don't have vim docs.
-  -- when the readme opens with :help it will be correctly displayed as markdown
-  readme = { enabled = false },
+  rocks = {enabled = false },
+  dev = { path = "~/Source" },
 })
--- vim:fdm=marker
