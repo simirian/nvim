84fd3848620b819e342b318e0050bf4d55675512
simirian <simirian5813@gmail.com>
2025-05-01 10:05:55 -0400

    refactor!: projects are keyed by name not path
    Projects actually need to have unique names, so now the projects are
    keyed by name.

--- lua/projects.lua
+++ lua/projects.lua
@@ -8,15 +8,6 @@ local H = {}
 --- @type table<string, string>
 H.projects = {}
 
---- Normalizes a path.
---- @param start string The first component of the path.
---- @vararg string Other components of the path.
---- @return string path
-function H.path(start, ...)
-  start = vim.fn.fnamemodify(start, ":p")
-  return vim.fs.normalize(vim.fs.joinpath(start, ...))
-end
-
 --- Saves the current project list to projects.json.
 function H.save()
   --- @diagnostic disable-next-line: undefined-field
@@ -48,33 +39,29 @@ function H.load()
 end
 
 --- Adds a new project at the path aliased to name.
---- @param path? string The path to the project directory, default cwd.
 --- @param name string Unique display name of the project.
-function M.add(path, name)
+--- @param path? string The path to the project directory, default cwd.
+function M.add(name, path)
   --- @diagnostic disable-next-line: undefined-field
-  path = H.path(path or vim.loop.cwd())
-  H.projects[path] = name
+  H.projects[name] = path or vim.loop.cwd()
   H.save()
 end
 
 --- Remove a project by its normalized path. This does not delete the project
 --- directory, it only causes this module to forget it exists.
---- @param path? string The path to the project to forget, default cwd.
-function M.remove(path)
-  --- @diagnostic disable-next-line: undefined-field
-  path = H.path(path or vim.loop.cwd())
-  H.projects[path] = nil
-  H.load()
+--- @param path? string The name of the project to remove, current is default.
+function M.remove(name)
+  H.projects[name] = nil
+  H.save()
 end
 
 --- Opens a project by name.
 --- @param name string The name of the project to open.
 function M.open(name)
-  for path, pname in pairs(H.projects) do
-    if pname == name then
-      vim.api.nvim_set_current_dir(path)
-      return
-    end
+  if H.projects[name] then
+      vim.api.nvim_set_current_dir(H.projects[name])
+  else
+    vim.notify("project " .. name .. " doesn't exist", vim.log.levels.ERROR, {})
   end
 end
 
@@ -83,18 +70,20 @@ end
 --- @param path? string The path to test.
 --- @return string? project
 function M.show(path)
-  --- @diagnostic disable-next-line: undefined-field
-  path = H.path(path or vim.loop.cwd() .. "/f")
-  return H.projects[H.path(vim.fs.root(path, function(_, rpath)
-    return H.projects[H.path(rpath)] ~= nil
-  end))]
+  path = vim.fs.normalize(path)
+  for name, ppath in pairs(H.projects) do
+    ppath = vim.fs.normalize(ppath)
+    if path == ppath or path:match("^" .. ppath:gsub("%%", "%%%%") .. "[/\\]") then
+      return name
+    end
+  end
 end
 
 --- Returs a list of all exisitng projects in no particular order.
 --- @return { path: string, name: string }[]
 function M.list()
   local projects = {}
-  for path, name in pairs(H.projects) do
+  for name, path in pairs(H.projects) do
     table.insert(projects, { name = name, path = path })
   end
   return projects
@@ -109,15 +98,7 @@ end
 --- @param opts? Projects.Opts
 function M.setup(opts)
   opts = opts or {}
-  if not opts.filepath then
-    H.filepath = H.path(vim.fn.stdpath("data") --[[@as string]], "projects.json")
-  else
-    if opts.filepath:match("[/\\]projects.json$") then
-      H.filepath = H.path(opts.filepath)
-    else
-      H.filepath = H.path(opts.filepath, "projects.json")
-    end
-  end
+  H.filepath = opts.filepath or vim.fn.stdpath("data") .. "/projects.json"
   H.load()
 
   vim.api.nvim_create_user_command("Project", function(args)
@@ -128,7 +109,7 @@ function M.setup(opts)
       --- @diagnostic disable-next-line: undefined-field
       M.remove(vim.loop.cwd())
     elseif args.fargs[1] == "open" then
-      M.open(args.fargs[1])
+      M.open(args.fargs[2])
     elseif args.fargs[1] == "show" then
       --- @diagnostic disable-next-line: undefined-field
       print(M.show(vim.loop.cwd()) or "none")
