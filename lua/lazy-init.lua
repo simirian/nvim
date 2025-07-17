1ebc12a1969124dad4321c6ee2ef9d27a7dc1ef6
simirian <simirian5813@gmail.com>
2025-07-17 16:42:09 -0400

    style: no longer using `vfn` local

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -1,12 +1,10 @@
 -- simirian's NeoVim
 -- lazy.nvim plugins
 
-local vfn = vim.fn
-
-local lazypath = vfn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 --- @diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-  vfn.system { "git", "clone", "--filter=blob:none",
+  vim.fn.system { "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, }
 end
 vim.opt.rtp:prepend(lazypath)
