adef0bbd3e3f15259691fed2ea4c99521f9c16bb
simirian <simirian5813@gmail.com>
2025-08-04 23:30:42 -0400

    refactor(treesitter): changed start method
    Instead of manually checking for a tree-sitter parser ourselves, we just
    use a `pcall()` on `vim.treesitter.start()` which will check for us. If
    it fails, then we just don't start treesitter and nothing bad happens
    (that I'm aware of).

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,12 +9,7 @@ return {
     require("nvim-treesitter").setup()
     vim.api.nvim_create_autocmd("FileType", {
       desc = "Enable treesitter in supported buffers.",
-      callback = function()
-        local language = vim.treesitter.language.get_lang(vim.bo.ft)
-        if vim.api.nvim_get_runtime_file("parser/" .. language .. "*", false)[1] then
-          vim.treesitter.start()
-        end
-      end,
+      callback = function() pcall(vim.treesitter.start) end,
     })
   end
 }
