3d802a7b22324c4db2f6accf0b7f7fb8f4df2520
simirian <simirian5813@gmail.com>
2025-08-21 21:48:28 -0400

    feat(calendir): full year calendar
    The `:Calendir` command can now take an integer as a year, and the
    plugin will open a buffer which displays every day in that year as
    columns under the months. Using the `gf` keymap on any of the dates will
    jump to that journal entry. Days with journal entries and the current
    date are also given special highlight groups.

--- plugin/calendir.lua
+++ plugin/calendir.lua
@@ -26,26 +26,111 @@ local function getcurrent()
   return { day = tonumber(day), month = tonumber(month), year = tonumber(year) }
 end
 
+--- Checks if a journal entry exists for the given day.
+--- @param date osdateparam The date of the entry to check.
+--- @return boolean
+local function exists(date)
+  local path = vim.fs.normalize(vim.g.calendir .. os.date("/%Y/%m/%d.md", os.time(date)))
+  --- @diagnostic disable-next-line: undefined-field
+  return vim.loop.fs_stat(path) ~= nil
+end
+
+--- The namepsace used by this plugin.
+--- @type integer
+local namespace = vim.api.nvim_create_namespace("calendir")
+
+--- Map from years to buffer numbers.
+--- @type table<integer, integer>
+local yearbuffers = {}
+
+--- Opens a calendar year buffer.
+--- @param year integer The year to open the calendar for.
+local function yearcalendar(year)
+  local bufnr = yearbuffers[year]
+  if not bufnr then
+    bufnr = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_name(bufnr, "calendir:/" .. year)
+    yearbuffers[year] = bufnr
+  end
+  vim.bo[bufnr].swapfile = false
+  vim.bo[bufnr].buftype = 'nofile'
+  vim.bo[bufnr].bufhidden = "hide"
+  vim.bo[bufnr].modifiable = true
+  local lines = { "" }
+  for month = 1, 12 do
+    lines[1] = lines[1] .. os.date(" %b ", os.time { year = year, month = month, day = 1 })
+  end
+  for day = 1, 31 do
+    local line = ""
+    for month = 1, 12 do
+      if os.date("*t", os.time { year = year, month = month, day = day }).month == month then
+        line = ("%s %3d "):format(line, day)
+      else
+        line = line .. "     "
+      end
+    end
+    table.insert(lines, line)
+  end
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.bo[bufnr].modified = false
+  vim.bo[bufnr].modifiable = false
+  vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+  for day = 1, 31 do
+    for month = 1, 12 do
+      if os.date("*t", os.time { year = year, month = month, day = day }).month == month then
+        local today = os.date("*t")
+        local hl
+        if year == today.year and month == today.month and day == today.day then
+          hl = exists{year = year, month = month, day = day} and "CalToday" or "CalNoToday"
+        else
+          hl = exists { year = year, month = month, day = day } and "CalDay" or nil
+        end
+        vim.api.nvim_buf_set_extmark(bufnr, namespace, day, month * 5 - 5, {
+          end_col = month * 5,
+          hl_group = hl
+        })
+      end
+    end
+  end
+  vim.keymap.set("", "gf", function()
+    local curpos = vim.api.nvim_win_get_cursor(0)
+    local month = math.floor(curpos[2] / 5) + 1
+    local day = curpos[1] - 1
+    open({year = year, month = month, day = day})
+  end, {buffer = bufnr, desc = "Open journal entry."})
+  vim.cmd("buffer! " .. bufnr)
+end
+
 vim.api.nvim_create_user_command("Calendir", function(args)
-  if args.fargs[1] == "today" then
+  if args.args == "today" then
     open(os.date("*t") --[[@as osdateparam]])
-  elseif args.fargs[1] == "yesterday" then
+  elseif args.args == "yesterday" then
     local date = os.date("*t")
     open { year = date.year, month = date.month, day = date.day - 1 }
-  elseif args.fargs[1] == "tomorrow" then
+  elseif args.args == "tomorrow" then
     local date = os.date("*t")
     open { year = date.year, month = date.month, day = date.day + 1 }
-  elseif args.fargs[1] == "previous" then
+  elseif args.args == "previous" then
     local date = getcurrent()
     if date then
       date.day = date.day - 1
       open(date)
     end
-  elseif args.fargs[1] == "next" then
+  elseif args.args == "next" then
     local date = getcurrent()
     if date then
       date.day = date.day + 1
       open(date)
+    end
+  else
+    local year = tonumber(args.args:sub(1, 4))
+    local month = tonumber(args.args:sub(6, 7))
+    if not year then
+      yearcalendar(tonumber(os.date("%Y")) --[[@as integer]])
+    elseif not month then
+      yearcalendar(year)
+    else
+      -- month calendar
     end
   end
 end, {
